e790c30778be85dcec23acef602cf08866957eb3
Mendozalz <mendoza124302@gmail.com>
2023-10-03 17:09:51 -0500

    feat:
    Corrigiendo TS

--- src/Converson.tsx
+++ src/Converson.tsx
@@ -1,50 +1,56 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const Conversor = () =>{
-    const [pesos, SetPesos] = useState(0)
-    const [divisa, setDivisa] = useState(0)
-    const cambios=[
-        {
-          moneda: "Euro",
-          cambio:0.00023,
-        },{
-          moneda: "Peso argentino",
-          cambio:0.084,
-        },{
-          moneda: "Peso mexicano",
-          cambio:0.0043,
-        },{
-          moneda: "Dólar",
-          cambio:0.00024
-        }
-      ]
-
-      const cambioPesos = (e) =>{
-            const valorPesos = e.target.value;
-            SetPesos(valorPesos);
-            const resultado = cambios.map(el => el.cambio * valorPesos);
-            setDivisa(resultado)
-      }
-
-    return(
-        <>
-        <div className="conversor-container">
-        <label htmlFor="u0">Pesos Colombiando</label>
-        <input id="u0" type="number" value={pesos} onChange={cambioPesos}/>
-        {
-            cambios.map((cambiar, i) =>{
-                return(
-                    <div className="conversor-container" key={i}>
-                        <label htmlFor={i}>{cambiar.moneda}</label>
-                        <input id={i} type="number" value={divisa[i]} readOnly/>
-                    </div>
-                )
-            } )
-        }
-        </div>
-        </>
-    )
+interface Cambio {
+  moneda: string;
+  cambio: number; 
 }
 
-export default Conversor
+const Conversor = () => {
+
+  const [pesos, setPesos] = useState(0);
+  const [divisas, setDivisas] = useState<number[]>([]);
+
+  const cambios: Cambio[] = [
+    { moneda: "Euro", cambio: 0.00023 },
+    { moneda: "Peso argentino", cambio: 0.084 },
+    { moneda: "Peso mexicano", cambio: 0.0043 },  
+    { moneda: "Dólar", cambio: 0.00024 }
+  ];
+
+  const cambioPesos = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valorPesos = Number(e.target.value);
+    setPesos(valorPesos);
+    
+    const resultados = cambios.map(c => c.cambio * valorPesos);
+    setDivisas(resultados);
+  }
+
+  return (
+    <>
+      <div className="conversor-container">
+        <label htmlFor="pesos">Pesos Colombianos</label>
+        <input 
+          id="pesos"
+          type="number" 
+          value={pesos}
+          onChange={cambioPesos} 
+        />
+
+        {cambios.map((cambio, i) => (
+          <div key={i} className="conversor-container">
+            <label htmlFor={cambio.moneda}>{cambio.moneda}</label>
+            <input
+              id={cambio.moneda}
+              type="number"
+              value={divisas[i]} 
+              readOnly 
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Conversor;
