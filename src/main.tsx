5fcb892bcc4c083b8443718638c3a42a996055a7
Mendozalz <mendoza124302@gmail.com>
2023-10-05 08:07:52 -0500

    fix: :bug:
    Eliminando ficheros innecesario que no permitian construir

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import Aficiones from "./Aficiones.tsx";
 import Conversor from "./Converson.tsx";
 import './index.css'
 
